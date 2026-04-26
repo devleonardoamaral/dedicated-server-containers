67601aef80f82ff07c5ac5f6efe41b709d558ff6
Leonardo Amaral <devleonardoamaral@gmail.com>
2026-04-26 09:29:49 -0300

    feat: improves the entrypoint to automatically configure the master's IP address.

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-cluster_name="${CLUSTER_NAME}"
+cluster_name="$CLUSTER_NAME"
 steamcmd_dir="$HOME/.steam/steam/steamcmd"
 install_dir="$HOME/.steam/steam/steamapps/common/Don't Starve Together Dedicated Server"
 dontstarve_dir="$HOME/.klei/DoNotStarveTogether/"
-
 cluster_dir="/$dontstarve_dir/$cluster_name"
 
-if [ ! -d  "$cluster_dir" ]; then
-    mkdir -p $cluster_dir
-fi
+function install_server_config()
+{
+    if [ ! -d  "$1" ]; then
+        mkdir -p "$1"
+    fi
 
-cp -rf /servercfg/* $cluster_dir
+    cp -rf /servercfg/* "$cluster_dir"
+
+    sed -Ei 's/^bind_ip =.*$/bind_ip = 172.25.0.2/' "$cluster_dir/cluster.ini"
+    sed -Ei 's/^bind_ip =.*$/bind_ip = 172.25.0.2/' "$cluster_dir/master_ip.ini"
+}
 
 function fail()
 {
@@ -26,14 +31,15 @@ function check_for_file()
 	fi
 }
 
+install_server_config "$cluster_dir"
+
 cd "$steamcmd_dir" || fail "Missing $steamcmd_dir directory!"
 
 check_for_file "steamcmd.sh"
-check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
-check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
-check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
-check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
-
+check_for_file "$cluster_dir/cluster.ini"
+check_for_file "$cluster_dir/cluster_token.txt"
+check_for_file "$cluster_dir/Master/server.ini"
+check_for_file "$cluster_dir/Caves/server.ini"
 check_for_file "$install_dir/bin64"
 
 cd "$install_dir/bin64" || fail
@@ -43,9 +49,9 @@ run_shared+=(-console)
 run_shared+=(-cluster "$cluster_name")
 # run_shared+=(-monitor_parent_process $$)
 
-if [ "${SERVER_TYPE}" == "Caves" ]; then
+if [ "$SERVER_TYPE" == "Caves" ]; then
     exec "${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /'
-elif [ "${SERVER_TYPE}" == "Master" ]; then
+elif [ "$SERVER_TYPE" == "Master" ]; then
     exec "${run_shared[@]}" -shard Master | sed 's/^/Master: /'
 else
     echo "Error: invalid SERVER_TYPE"

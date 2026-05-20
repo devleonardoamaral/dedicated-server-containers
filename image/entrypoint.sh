6d3fda43e10b7dfaf095d08e3d905ac6ab13ce60
Leonardo Amaral <devleonardoamaral@gmail.com>
2026-05-20 12:41:50 -0300

    feat: centralized configuration via the .env file.

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-volume_dir="/opt/Minecraft"
-server_dir="$volume_dir/servers/$SERVER_NAME"
+image_dir="/opt/Minecraft/image"
+server_dir="/opt/Minecraft/servers/$SERVER_NAME"
 
 # Install
 mkdir -p "$server_dir"
-cp -rf $volume_dir/image/* "$server_dir"
-
-# Configure
-echo "-Xms${SERVER_XMS} -Xmx${SERVER_XMX} ${SERVER_ARGS}" > "$user_jvm_args.txt"
+cp -rf $image_dir/* "$server_dir"
 
 # Execute
 cd "$server_dir"
-exec java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.228/unix_args.txt -nogui "$@"
+exec java -Xms${JVM_XMS} -Xmx${JVM_XMX} ${JVM_ARGS} @libraries/net/neoforged/neoforge/${NEOFORGE_VERSION}/unix_args.txt "$@"

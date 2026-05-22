03befd31222a672b6903f8c7451527f63c30de46
Leonardo Amaral <devleonardoamaral@gmail.com>
2026-05-22 14:11:24 -0300

    feat: add setting to define the Java version

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -3,10 +3,8 @@
 image_dir="/opt/Minecraft/image"
 server_dir="/opt/Minecraft/servers/$SERVER_NAME"
 
-# Install
 mkdir -p "$server_dir"
 cp -rf $image_dir/* "$server_dir"
 
-# Execute
 cd "$server_dir"
 exec java -Xms${JVM_XMS} -Xmx${JVM_XMX} ${JVM_ARGS} @libraries/net/neoforged/neoforge/${NEOFORGE_VERSION}/unix_args.txt "$@"
